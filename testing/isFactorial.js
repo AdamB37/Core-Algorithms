1c1bad9cdafdb3874070b1d042458cd6e2c6faea
AdamB37 <adam.beshir@gmail.com>
2017-01-12 15:18:04 -0800

    mergeSort is completed

--- testing/isFactorial.js
+++ testing/isFactorial.js
@@ -2,15 +2,15 @@ import { expect } from 'chai'
 import isFactorial from '../functions/isFactorial'
 
 describe('isFactorial', () => {
-  context('when given a number', () => {
-    it('should return the isFactorial', () => {
-      expect(isFactorial(5)).to.equal(120)
-    })
-  });
 
-  context('when given a negetive number', () =>{
-    it('should retrn an undefined', () => {
-      expect(isFactorial(-5)).to.equal(undefined)
-    })
+  it('returns 120 (factorial of 5)', () => {
+    expect(isFactorial(5)).to.equal(120)
   })
+
+
+
+  it('returns undefined for negative integer', () => {
+    expect(isFactorial(-5)).to.equal(undefined)
+  })
+
 });
--- testing/setUnion.js
+++ testing/setUnion.js
@@ -2,9 +2,7 @@ import { expect } from 'chai'
 import setUnion from '../functions/setUnion'
 
 describe('setUnion()', function(){
-  context('Take two sets of arrays', function(){
-    it('should return the union of the two arrays', function(){
-      expect(setUnion([1, 2, 3, 4], [2, 4, 6, 8])).to.eql([1, 2, 3, 4, 6, 8])
-    })
+  it('return the union of the two arrays', function(){
+    expect(setUnion([1, 2, 3, 4], [2, 4, 6, 8])).to.deep.equal([1, 2, 3, 4, 6, 8])
   })
 })
